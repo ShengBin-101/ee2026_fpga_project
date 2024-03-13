f47bc47535491aa9c41998bc45152c87d4c061b9
Sheng Bin <52733750+ShengBin-101@users.noreply.github.com>
2024-03-13 12:42:02 +0800

    Change from JB to JC port for OLED

--- MODS.srcs/sources_1/new/Top_Student.v
+++ MODS.srcs/sources_1/new/Top_Student.v
@@ -16,14 +16,18 @@ module Top_Student (
     input basys_clk,
     input btnU, btnL, btnR, btnD, btnC, 
     input [15:0] SW,
-    output [7:0] JB, 
+//    output [7:0] JB, 
+    output [7:0] JC,
     output [15:0] led,
     output [6:0] seg,
     output [3:0] an,
+    output dp,
     inout PS2Clk, PS2Data
 );
 
     assign an = 4'b0000;
+    assign dp = 1;
+    assign JC[2] = 0; // Some boards require JC[2] to be cleared to 0 for OLED to work
 
     // ===============OLED====================
     wire [15:0] oled_data;
@@ -41,13 +45,13 @@ module Top_Student (
         .sample_pixel(sample_pixel), 
         .pixel_index(pixel_index), 
         .pixel_data(oled_data), 
-        .cs(JB[0]), 
-        .sdin(JB[1]), 
-        .sclk(JB[3]), 
-        .d_cn(JB[4]), 
-        .resn(JB[5]), 
-        .vccen(JB[6]),
-        .pmoden(JB[7]));
+        .cs(JC[0]), 
+        .sdin(JC[1]), 
+        .sclk(JC[3]), 
+        .d_cn(JC[4]), 
+        .resn(JC[5]), 
+        .vccen(JC[6]),
+        .pmoden(JC[7]));
 
     task_d task_d_module(.basys_clk(basys_clk), .SW0(SW[0]), .btnC(btnC), .btnL(btnL), .btnR(btnR), .btnU(btnU), .btnD(btnD), .pixel_index(pixel_index) , .oled_data(oled_data));
 
